deddbdb10932708c280a06bddd4111dcb6e58164
Daniel Faulkner <dfaulkner707@gmail.com>
2023-09-10 21:20:58 -0700

    eh yeah commit time

--- src/HiddenSecrets.java
+++ src/HiddenSecrets.java
@@ -8,7 +8,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-// PUT YOUR IMPORTS HERE
+import java.nio.file.Paths;
+   import     java.util.Scanner;
+      import  java.nio.file.Path;
 
 public class HiddenSecrets {
     public static void getHiddenSecrets(File file) {
@@ -37,10 +39,10 @@ public class HiddenSecrets {
     }
 
     public static void main(String[] args) {
-        // Put your code to request a file path,
-        // read in a string from System.in,
-        // convert that string into A Path type using Paths class,
-        // and call the getHiddenSecrets method to get the file's meta-data
-        // HERE
+        Scanner fileYes = new Scanner(System.in);
+        String hotdog = fileYes.nextLine();
+        Path path = Path.of(hotdog);
+        getHiddenSecrets(path.toFile());
+        System.out.println();
     }
 }
